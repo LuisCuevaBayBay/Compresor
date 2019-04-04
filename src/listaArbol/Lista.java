c07f28933d3deee98c90f838166f1d0f686ba005
Luis Cueva <luis.cuevas@ujcv.edu.hn>
2019-04-04 08:20:17 -0600

    2nd commit

---
+++ src/listaArbol/Lista.java
@@ -0,0 +1,4 @@
+package listaArbol;
+
+public class Lista {
+}
--- src/tabla/ListaTabla.java
+++ src/tabla/ListaTabla.java
@@ -1,10 +1,9 @@
 package tabla;
 
 import excepciones.ListaVacia;
-import listaArbol.Lista;
+
 
 public class ListaTabla {
-
     private Nodo inicio;
 
     public Nodo getInicio() {
@@ -15,36 +14,67 @@ public class ListaTabla {
         this.inicio = inicio;
     }
 
-    public void agregar(String valor, String ubicacion) {
-        Nodo nuevo = new Nodo(valor, ubicacion);
-        if (this.getInicio() == null)
-            this.setInicio(nuevo);
-        else {
-            nuevo.setSiguiente(inicio);
-            inicio = nuevo;
-        }
+    public ListaTabla(){
+        inicio=null;
     }
 
+    public void agregar(String valor,String ubicacion){
+        Nodo nuevo=new Nodo(valor,ubicacion);
+        if(this.getInicio() == null)
+            this.setInicio(nuevo);
+        else{
+            nuevo.setSiguiente(inicio);
+            inicio=nuevo;
+        }
+    }
     public Nodo sacarInicio() throws ListaVacia {
-        if (this.inicio == null)
+        if(this.inicio==null)
             throw new ListaVacia();
-        Nodo aux = inicio;
-        inicio = aux.getSiguiente();
+        Nodo aux=inicio;
+        inicio=aux.getSiguiente();
         aux.setSiguiente(null);
         return aux;
     }
 
-    public void Imprimir() {
-        if (this.getInicio() != null) {
-            Nodo aux = inicio;
-            while (aux != null) {
-                System.out.println(aux.getDato() + ":" + aux.getUbicacion());
-                aux = aux.getSiguiente();
+    public void imprimir(){
+        if(this.getInicio()!=null){
+            Nodo aux=inicio;
+            while(aux!=null){
+                System.out.println(aux.getDato()+":"+aux.getUbicacion());
+                aux=aux.getSiguiente();
             }
         }
+    }
 
+    public String unirTodo(String mensaje) throws ListaVacia {
+        if(this.getInicio()==null)
+            throw new ListaVacia();
+        String todo="";
+        for(int i=0;i<mensaje.length();i++)
+            todo=todo+ubicacionDe(mensaje.charAt(i)+"");
+        return todo;
+    }
+
+    private String ubicacionDe(String dato){
+        Nodo aux=inicio;
+        while(aux!=null && !(aux.getDato().equals(dato))){
+            aux=aux.getSiguiente();
+        }
+        return aux.getUbicacion();
+    }
+
+    public String LetraDe(String ubicacion){
+        String letra=null;
+        Nodo aux=inicio;
+        while(aux!=null && !(aux.getUbicacion().equals(ubicacion))){
+            aux=aux.getSiguiente();
+        }
+        if(aux!=null)
+            letra=aux.getDato();
+        return letra;
+    }
 
     }
-}
+
 
 
